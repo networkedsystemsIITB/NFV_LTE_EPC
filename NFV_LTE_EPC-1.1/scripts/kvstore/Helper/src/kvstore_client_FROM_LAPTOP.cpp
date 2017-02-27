4cc3033e920792eb4b868891cac6e3b971400c97
PRATIK SATAPATHY <pratiksatapathy@gmail.com>
2017-02-27 11:42:04 +0530

    Add files via upload

---
+++ NFV_LTE_EPC-1.1/scripts/kvstore/Helper/src/kvstore_client_FROM_LAPTOP.cpp
@@ -0,0 +1,165 @@
+#ifndef __KVSTORE_CLIENT_H__
+#define __KVSTORE_CLIENT_H__
+
+//TODO: Replace this with MessageHandler
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define MAX_INPUT_SIZE 1048576 //1MB
+
+
+using namespace std;
+inline void itoba(uint32_t i,char *ba){
+  ba[0]=(i>>24)&255;
+  ba[1]=(i>>16)&255;
+  ba[2]=(i>>8)&255;
+  ba[3]=(i)&255;
+}
+
+inline uint32_t batoi(char *ba){
+  uint32_t i=0;
+  int iba[4];
+  iba[0]=ba[0];
+  iba[1]=ba[1];
+  iba[2]=ba[2];
+  iba[3]=ba[3];
+  i=(uint32_t)((iba[0]&255)<<24)|((iba[1]&255)<<16)|((iba[2]&255)<<8)|(iba[3]&255);
+}
+
+class KVStoreClient{
+private:
+  uint32_t num_bytes;
+  char op_code;
+  int portnum;
+  string ip;
+  int sockfd, n;
+  struct sockaddr_in server_addr;
+  char inputbuf[MAX_INPUT_SIZE],*more_buf;
+  char outbuf[MAX_INPUT_SIZE];
+
+public:
+  KVStoreClient(string ip_addr,int port){
+    ip=ip_addr;
+    portnum=port;
+    /* Create client socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+      fprintf(stderr, "ERROR opening socket\n");
+      exit(1);
+    }
+
+    /* Fill in server address */
+    bzero((char *) &server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    if(!inet_aton(ip.c_str(), &server_addr.sin_addr))
+    {
+      fprintf(stderr, "ERROR invalid server IP address\n");
+      exit(1);
+    }
+    server_addr.sin_port = htons(portnum);
+
+    /* Connect to server */
+    if (connect(sockfd,(struct sockaddr *)&server_addr,sizeof(server_addr)) < 0)
+    {
+      fprintf(stderr, "ERROR connecting\n");
+      exit(1);
+    }
+    //printf("Connected to server\n");
+  }
+
+void send(vector<string> &v){
+  int len=v.size();
+  int sum=1; //First byte as Number of stings
+  for(int i=0;i<len;i++){
+    sum+=(v[i].size()+4);
+  }
+  int idx=0;
+  char *data=outbuf;
+  if(MAX_INPUT_SIZE<sum){
+	  cout<<"Alloc data sum="<<sum<<endl;
+   data = new char[sum];
+  }
+  data[idx]=(char)len;
+  idx++;
+  for(int i=0;i<len;i++){
+    itoba((uint32_t)v[i].size(),data+idx);
+    idx+=4;
+    strncpy(data+idx,v[i].c_str(),v[i].size());
+    idx+=v[i].size();
+  }
+  n=-1;
+  int sent=0;
+  while(sent<sum){
+  n = write(sockfd,data+sent,sum-sent);
+
+	if (errno == EPERM) {
+		errno = 0;
+		usleep(1000);
+		continue;
+	}
+	else if(n<0) {
+	    fprintf(stderr, "ERROR writing to socket %d\n",errno);
+	    exit(1);
+//		break;
+	}
+//  if (n < 0)
+//  {
+//    fprintf(stderr, "ERROR writing to socket\n",);
+//    exit(1);
+//  }
+  sent+=n;
+  }
+  if(MAX_INPUT_SIZE<sum){
+
+	  cout<<"Delete data sum="<<sum<<endl;
+   delete(data);
+  }
+}
+
+vector<string> receive(){
+  n = read(sockfd,inputbuf,1);
+  if (n < 0)
+  {
+    fprintf(stderr, "ERROR reading from socket\n");
+    exit(1);
+  }
+  int len=(int)inputbuf[0];
+  std::vector<string> v(len);
+  for(int i=0;i<len;i++){
+    n = read(sockfd,inputbuf,4);
+    if (n < 0)
+    {
+      fprintf(stderr, "ERROR reading from socket\n");
+      exit(1);
+    }
+    int sz=(int)batoi(inputbuf);
+    int sumn=0;
+    while(sumn!=sz){
+      n = read(sockfd,inputbuf+sumn,sz-sumn);
+      sumn+=n;
+      //printf("SZ:%d  N:%d\n",sz,n);
+      if (n < 0)
+      {
+        fprintf(stderr, "ERROR reading from socket\n");
+        exit(1);
+      }
+    }
+    v[i]=string(inputbuf,sz);
+    // cout<<v[i]<<endl;
+  }
+  return v;
+}
+};
+#endif
---
+++ NFV_LTE_EPC-1.1/scripts/kvstore/Helper/src/server_system_controler.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+  "os"
+  // "errors"
+  "os/exec"
+  "fmt"
+  "net"
+  // "strconv"
+  "time"
+  // "strings"
+)
+
+func temp_includes(){
+  time.Now();
+}
+
+
+func itoba(i int) []byte {
+    slen := i
+    bytearr := make([]byte,4)
+    idx:=0
+    bytearr[idx] = byte((slen >> 24) & 255)
+    idx++
+    bytearr[idx] = byte((slen >> 16) & 255)
+    idx++
+    bytearr[idx] = byte((slen >> 8) & 255)
+    idx++
+    bytearr[idx] = byte((slen) & 255)
+    idx++
+    return bytearr
+}
+
+func batoi(bytearr []byte) int{
+  idx:=4
+  x:=int(bytearr[idx-4])<<24 + int(bytearr[idx-3])<<16 + int(bytearr[idx-2])<<8 + int(bytearr[idx-1])
+  return x
+}
+
+
+func writeStrings(strarr []string,conn net.Conn){
+  alen := len(strarr)
+  sum := 1
+  for i:=0;i<alen;i++ {
+    sum += (len(strarr[i])+4)
+  }
+  //fmt.Println("Sum",sum)
+  bytearr := make([]byte,sum)
+  idx := 0
+  bytearr[0] = byte(alen)
+  idx++
+  for i:=0;i<alen;i++ {
+    slen := len(strarr[i])
+    bytearr[idx] = byte((slen >> 24) & 255)
+    idx++
+    bytearr[idx] = byte((slen >> 16) & 255)
+    idx++
+    bytearr[idx] = byte((slen >> 8) & 255)
+    idx++
+    bytearr[idx] = byte((slen) & 255)
+    idx++
+
+    copy(bytearr[idx:],[]byte(strarr[i]))
+    idx+=slen
+
+  }
+  n,err := conn.Write(bytearr) //? err
+  if n<sum {
+    fmt.Println("Write error",err);
+  }
+}
+
+func readString(conn net.Conn) string{
+  lenbuf := make([]byte,4)
+  /*n,err := */conn.Read(lenbuf) //? err
+  var slen int
+  slen = int(lenbuf[0])<<24 + int(lenbuf[1])<<16 + int(lenbuf[2])<<8 + int(lenbuf[3])
+  //fmt.Println("Slen:",slen)
+  strbuf := make([]byte,slen)
+  dsum:=0;
+  for dsum!=slen{
+    n,_ := conn.Read(strbuf[dsum:]) //? err
+    dsum+=n
+  }
+  return string(strbuf)
+}
+
+func readStrings(conn net.Conn) ([]string, error) {
+  no_of_string := make([]byte,1)
+  _,err := conn.Read(no_of_string) //? err
+  if(err!=nil){
+    return nil,err
+  }
+  strarr := make([]string,no_of_string[0])
+  for i:=byte(0);i<no_of_string[0];i++ {
+    strarr[i] = readString(conn)
+  }
+  return strarr,nil
+}
+
+
+func syncSystem(cmd string) ([]byte,error) {
+    return exec.Command("sh","-c",cmd).Output()
+}
+
+func asyncSystem(cmd string){
+  go func(cmd string){
+    _, err := exec.Command("sh","-c",cmd).Output()
+    //fmt.Println(string(out))
+    if err!=nil{
+      fmt.Println("Error executing :",cmd,"\n",err)
+    }
+  }(cmd)
+}
+
+func handleClient(conn net.Conn) {
+  // var cl Client
+  //i := 0
+  for {
+    strarr,err := readStrings(conn)
+    if err!=nil {
+      fmt.Println("Error")
+      return
+    }
+    for _,cmd := range strarr {
+          asyncSystem(cmd)
+    }
+  }
+}
+
+
+func main() {
+  // asyncSystem("sar -o tempop -u 1");
+  // time.Sleep(10000 * time.Millisecond);
+  // asyncSystem("pkill -SIGINT sar");
+  // time.Sleep(5000 * time.Millisecond);
+  // fmt.Println("Done");
+
+  socket := os.Args[1] //Read form command line arugment
+  ln, err := net.Listen("tcp",socket)
+
+  if err != nil {
+      panic(err)
+      return
+    } else {
+      fmt.Println("System Controller Started @ ",os.Args[1])
+      for{
+        conn, err := ln.Accept()
+        if err != nil {
+          continue
+        }
+        go handleClient(conn)
+      }
+    }
+  }
